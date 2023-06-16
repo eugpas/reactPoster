de2ebb637391d6f9c92eecb40a30797aa5d3cc2d
Yevhen Paschenko <ypasch@softserveinc.com>
2023-06-16 20:01:08 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="Eugene" body="React.JS"/>
+      <Post author="Solomiia" body="WildRift"/>
+    </main>
+  );
+}
+
+export default App;
